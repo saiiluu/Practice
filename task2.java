da24b94834d8ad80d064b0c24ec4ea98575c6d20
Ajit Bhujbal <saiiluu1370@gmail.com>
2026-01-18 23:44:30 +0530

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class studentInfo()
+  {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter your name");
+  String name=sc.next();
+  System.out.println("enter the age");
+  int age=sc.nextInt();
+  System.out.println("enter marks");
+  float marks=sc.nextInt();
+  public static void main(String args[])
+    {
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(marks);
+  }
+
+}
