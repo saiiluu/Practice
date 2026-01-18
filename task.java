8ae49550c66a162af4e71d8e0e51ec1e33de8c33
Ajit Bhujbal <saiiluu1370@gmail.com>
2026-01-18 23:32:40 +0530

    Create task.java

---
+++ task.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class votting()
+{
+int age=20;
+public static void main(String args[])
+{
+if(age<=18)
+{
+System.out.println("your not eligible for voting");
+}
+else
+System.out.println("your are eligible for votting");
+}
+}
